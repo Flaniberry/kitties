beeb7ac8104cdd07b98a02808d3aff36b1147d9c
Flaniberry <130238115+Flaniberry@users.noreply.github.com>
2023-07-02 12:55:21 -0400

    Secret code: more complicated
    Reversed output, fixed formatting, added an eraser to input. output now clears when input changes. Optimized JS

--- code/app.js
+++ code/app.js
@@ -1,55 +1,42 @@
-"use strict";
+'use strict';
 
 const btnEncode = document.querySelector('#btnEncode');
 const btnDecode = document.querySelector('#btnDecode');
 const txtInput = document.querySelector('#txtInput');
 const txtOutput = document.querySelector('#txtOutput');
 const clipboard = document.querySelector('#clipboard');
-const offsetForLetters = 21; // num between 0 (no change) and 25
+const eraser = document.querySelector('#eraser');
+const offsetForLetters = 19; // num between 0 (no change) and 25
 
 // 65 - 90  A to Z
 // 97 - 122 a to z
 // receives a letter (upper or lower case) and returns an offset letter (must be between zero and 25)
-function encode(c, offset) {
+function enOrDeCodeChar(c, offset) {
   const asciiVal = c.charCodeAt(0);
-  let newAsciiVal = 0;
-  if ('A' <= c && c <= 'Z')
-    newAsciiVal = (((asciiVal + offset) - 65) % 26) + 65;
-  else // lowercase letter
-    newAsciiVal = (((asciiVal + offset) - 97) % 26) + 97;
-    
+  let newAsciiVal;
+  if ('A' <= c && c <= 'Z') newAsciiVal = ((asciiVal + offset - 65) % 26) + 65;
+  // lowercase letter
+  else newAsciiVal = ((asciiVal + offset - 97) % 26) + 97;
+
   return String.fromCharCode(newAsciiVal);
 }
 
-function decode(c, offset) {
-  const asciiVal = c.charCodeAt(0);
-  let newAsciiVal = 0;
-  if ('A' <= c && c <= 'Z')
-    newAsciiVal = (((asciiVal - offset + 26) - 65) % 26) + 65;
-  else // lowercase letter
-    newAsciiVal = (((asciiVal - offset + 26) - 97) % 26) + 97;
-    
-  return String.fromCharCode(newAsciiVal);
+function codeBtnHelper(offset) {
+  txtOutput.value = '';
+  for (const c of txtInput.value) {
+    if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+      txtOutput.value = enOrDeCodeChar(c, offset) + txtOutput.value;
+    else txtOutput.value = c + txtOutput.value;
+  }
 }
 
 btnEncode.addEventListener('click', (e) => {
-  txtOutput.value = "";
-  for (const c of txtInput.value) {
-    if(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-      txtOutput.value = txtOutput.value + encode(c, offsetForLetters);
-    else
-      txtOutput.value = txtOutput.value + c;
-  }
+  codeBtnHelper(offsetForLetters);
 });
 
+// to decode reverse the offset: 26-offset
 btnDecode.addEventListener('click', (e) => {
-  txtOutput.value = "";
-  for (const c of txtInput.value) {
-    if(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-      txtOutput.value = txtOutput.value + decode(c, offsetForLetters);
-    else
-      txtOutput.value = txtOutput.value + c;
-  }
+  codeBtnHelper(26 - offsetForLetters);
 });
 
 // copy output text to clipboard
@@ -57,3 +44,12 @@ clipboard.addEventListener('click', (e) => {
   navigator.clipboard.writeText(txtOutput.value);
 });
 
+// erase text from first text area
+eraser.addEventListener('click', (e) => {
+  txtInput.value = '';
+});
+
+// clear output every time there is an edit to input
+txtInput.addEventListener('input', (e) => {
+  txtOutput.value = '';
+});
